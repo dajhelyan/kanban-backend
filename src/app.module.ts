a0d4baca44f46efff07a552f05266af2e79ce5c6
dajhelyan <assiaxramx10@gmail.com>
2023-06-05 14:27:56 -0500

    Add GraphQL set up

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,19 @@ import { TaskModule } from './task/task.module';
 import { TaskService } from './task/task.service';
 import { TaskController } from './task/task.controller';
 import { PrismaModule } from './prisma/prisma.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: `.${process.env.NODE_ENV}.env`,
       isGlobal: true
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
     TaskModule,
     PrismaModule
