1822ae90ca74f4cfc82801027eafd5ee81ecca9b
dajhelyan <assiaxramx10@gmail.com>
2023-06-10 12:55:43 -0500

    Add jwt module to generate access tokens

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,14 +4,26 @@ import { AuthResolver } from './auth.resolver';
 import { UserService } from 'src/user/user.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { PrismaService } from 'src/prisma/prisma.service';
-
+import { JwtModule, JwtService } from '@nestjs/jwt'
+import { ConfigModule, ConfigService } from '@nestjs/config';
 @Module({
+  imports:[
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: '24h',
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  exports:[JwtModule],
   providers: [
     AuthResolver, 
     AuthService, 
     UserService, 
-    PrismaModule, 
-    PrismaService
-  ]
+    PrismaService, 
+    ]
 })
 export class AuthModule {}
