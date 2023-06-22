17ba5f054d429d2e63df6994c5a89d89cadf330f
dajhelyan <assiaxramx10@gmail.com>
2023-06-22 12:19:13 -0500

    Add custom decorator to get current user

---
+++ src/auth/strategy/current-user.decorator.ts
@@ -0,0 +1,12 @@
+import { ExecutionContext, createParamDecorator } from "@nestjs/common";
+import { GqlExecutionContext } from "@nestjs/graphql";
+
+
+export const CurrentUser = createParamDecorator(
+  (data: string, context: ExecutionContext) => {
+    console.log(context, 'data')
+    const ctx = GqlExecutionContext.create(context);
+    const user = ctx.getContext().req.user;
+    return data ? user?.[data] : user;
+  },
+)
